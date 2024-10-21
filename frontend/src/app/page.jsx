feed684f6240e71102eeebbc880a1b46f55e5197
AyushG <legendsteamprivate@gmail.com>
2024-10-21 14:57:51 +0530

    Home page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center m-0'>
-      <video autoPlay muted loop playsInline src="Homevideo.mp4" id='backgroundHome'></video>
+    <div className='flex flex-col items-center gap-0 m-0'>
+      <div className='h-[90vh] w-full'>
+        <video autoPlay muted loop playsInline src="Homevideo.mp4" id='backgroundHome'></video>
 
-      <div className='absolute top-[70vh] left-[10vh]' id='homeContent'>
-        <h1 className='text-3xl italic font-bold text-white'>Gearshift Garages</h1>
-        
+        <div className='absolute top-[70vh] left-[10vh]' id='homeContent'>
+          <h1 className='text-3xl italic font-bold text-white'>Gearshift Garages</h1>
+        </div>
       </div>
+
+      <div className='h-[100vh] bg-black w-full m-0'>
+        <h1>Hello</h1>
+      </div>
+      
     </div>
   )
 }
