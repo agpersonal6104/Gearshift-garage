72f8454f2a2a5654084eaa4a12b0eb9936088666
AyushG <legendsteamprivate@gmail.com>
2024-10-21 14:41:07 +0530

    Home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 'use client';
+import Link from 'next/link';
 import React from 'react';
 <link href="https://fonts.googleapis.com/css?family=Bruno+Ace+SC:regular" rel="stylesheet" />
 const Navbar = () => {
   return (
     <nav className="text-white navbar h-[8vh]">
       <div className="w-full mx-auto">
-        <div className="flex items-center w-full m-0">
-          <ul className="flex justify-start w-1/3 gap-8 navbar-menu">
-            <li><a href="#" className="text-2xl hover:text-gray-300">Brands</a></li>
-            <li><a href="#" className="text-2xl hover:text-gray-300">Images</a></li>
+        <div className="grid items-center w-full grid-cols-3 m-0">
+          <ul className="flex justify-start gap-8 navbar-menu">
+            <li><Link href="#" className="text-2xl hover:text-gray-300">Brands</Link></li>
+            <li><Link href="#" className="text-2xl hover:text-gray-300">Images</Link></li>
           </ul>
-          <div className="flex items-center justify-center w-1/3 navbar-logo">
+          <div className="flex items-center justify-center navbar-logo">
             <h1 className='text-5xl font-bold'>Gearshift Garage</h1>
           </div>
-          <div className="flex items-center justify-end w-1/3 gap-12 search-bar">
+          
+          <div className='flex lg:hidden'>
+          </div>
+          
+          <div className="flex items-center justify-end gap-12 search-bar navbar-menu">
             
             <ul>
-              <li><a href="#" className='text-2xl font-bold hover:text-gray-300'>Compare</a></li>
+              <li><Link href="#" className='text-2xl font-bold hover:text-gray-300'>Compare</Link></li>
             </ul>
 
             <button type="submit" className="px-3 py-2 rounded-md" id='navbarSearchBtn'>
