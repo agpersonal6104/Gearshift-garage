2f5a85197e6018b412aad1e1bd42ce598f8628ba
AyushG <legendsteamprivate@gmail.com>
2024-12-15 23:17:31 +0530

    Section 3

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -26,6 +26,44 @@ const Home = () => {
       </div>
 
       <BrandMenu />
+
+      <div className='w-full m-0'>
+        
+        <h2 className="px-4 text-4xl font-bold text-left text-white">CATEGORIES</h2>
+      
+        <div className='w-full'>
+          
+          <div className='flex h-[45vh]'>
+            
+            <div className='w-1/3 px-2'></div>
+            
+            <div className='w-2/3 px-6 border-l-2 border-white'>
+            <h2 className='pt-4 pb-16 text-5xl text-white underline'>1-0-1</h2>
+            <p className='py-8 text-5xl italic text-white'>SMALL DESCRIPTION ON 1-0-1 MODELS.</p>
+            </div>
+            
+          </div>
+          
+        </div>
+        
+        <div className='w-full'>
+          
+          <div className='flex h-[45vh]'>
+            
+            <div className='w-2/3 px-6 border-r-2 border-white'>
+            <h2 className='pt-4 pb-16 text-5xl text-right text-white underline'>CLASSIC</h2>
+            <p className='py-8 text-5xl italic text-right text-white'>SMALL DESCRIPTION ON CLASSIC MODELS.</p>
+            </div>
+            
+            <div className='w-1/3 px-6 mx-auto my-auto'>
+              <img src="Ferrari250GTO.jpg" alt="Ferrari250GTO" className='rounded-2xl' />
+            </div>
+            
+          </div>
+          
+        </div>
+
+      </div>
       
     </div>
   )
