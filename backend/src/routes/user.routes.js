d153c3aee25d6a5102a9b98e187232f685837bbd
Evie Frye <msmanika763348@gmail.com>
2025-01-05 13:18:42 +0530

    Login and logout working.

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register")
 .post(upload.single('profile_pic'), registerUser);
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router;
